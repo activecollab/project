fefbd712455ef5da78be20e43cec1786030f14f7
Ilija Studen <ilija.studen@activecollab.com>
2016-05-09 00:55:12 +0200

    Add structure to the application

---
+++ app/src/Model/Structure.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the App project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+namespace ActiveCollab\App\Model;
+
+use ActiveCollab\DatabaseStructure\Structure as BaseStructure;
+
+/**
+ * @package ActiveCollab\App\Model
+ */
+class Structure extends BaseStructure
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+    }
+}
