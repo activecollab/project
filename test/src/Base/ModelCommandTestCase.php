aa9aaa155ca93d573c495a67192d5f67da4bb543
Ilija Studen <ilija.studen@activecollab.com>
2016-05-17 09:39:16 +0200

    Add project level ModelCommandTestCase

---
+++ test/src/Base/ModelCommandTestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the App project.
+ *
+ * (c) My Company <info@example.com>. All rights reserved.
+ */
+
+namespace ActiveCollab\App\Test\Base;
+
+use ActiveCollab\Bootstrap\TestCase\ModelCommandTestCase as BaseModelCommandTestCase;
+
+/**
+ * @package ActiveCollab\App\Test\Base
+ */
+abstract class ModelCommandTestCase extends BaseModelCommandTestCase
+{
+    use TestCaseTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getModelNamespace()
+    {
+        return '\\ActiveCollab\\App\\Model';
+    }
+}
